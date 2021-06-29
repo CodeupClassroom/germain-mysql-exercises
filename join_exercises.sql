cf850a91b8aeff5cfa7625ad5fea51742c4df7d1
Ryan Orsinger <ryan.orsinger@gmail.com>
2021-06-29 17:02:41 -0500

    add quickfix solution for the second bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -93,3 +93,87 @@ join employees on dept_emp.emp_no = employees.emp_no
 where dept_manager.to_date > curdate()
 and dept_emp.to_date > curdate();
 
+# The quick fix solution for #11 Bonus
+(select dept_no, salary, emp_no, first_name, last_name
+from salaries
+join dept_emp using(emp_no)
+join employees using(emp_no)
+where dept_no = 'd001'
+order by salary DESC
+limit 1)
+union
+(select dept_no, salary, emp_no, first_name, last_name
+from salaries
+join dept_emp using(emp_no)
+join employees using(emp_no)
+where dept_no = 'd002'
+order by salary DESC
+limit 1)
+union
+(select dept_no, salary, emp_no, first_name, last_name
+from salaries
+join dept_emp using(emp_no)
+join employees using(emp_no)
+where dept_no = 'd003'
+order by salary DESC
+limit 1)
+union
+(select dept_no, salary, emp_no, first_name, last_name
+from salaries
+join dept_emp using(emp_no)
+join employees using(emp_no)
+where dept_no = 'd004'
+order by salary DESC
+limit 1)
+union
+(select dept_no, salary, emp_no, first_name, last_name
+from salaries
+join dept_emp using(emp_no)
+join employees using(emp_no)
+where dept_no = 'd005'
+order by salary DESC
+limit 1)
+union
+(select dept_no, salary, emp_no, first_name, last_name
+from salaries
+join dept_emp using(emp_no)
+join employees using(emp_no)
+where dept_no = 'd006'
+order by salary DESC
+limit 1)
+union
+(select dept_no, salary, emp_no, first_name, last_name
+from salaries
+join dept_emp using(emp_no)
+join employees using(emp_no)
+where dept_no = 'd006'
+order by salary DESC
+limit 1)
+union
+(select dept_no, salary, emp_no, first_name, last_name
+from salaries
+join dept_emp using(emp_no)
+join employees using(emp_no)
+where dept_no = 'd007'
+order by salary DESC
+limit 1)
+union
+(select dept_no, salary, emp_no, first_name, last_name
+from salaries
+join dept_emp using(emp_no)
+join employees using(emp_no)
+where dept_no = 'd008'
+order by salary DESC
+limit 1)
+union
+(select dept_no, salary, emp_no, first_name, last_name
+from salaries
+join dept_emp using(emp_no)
+join employees using(emp_no)
+where dept_no = 'd009'
+order by salary DESC
+limit 1);
+
+
+
+
